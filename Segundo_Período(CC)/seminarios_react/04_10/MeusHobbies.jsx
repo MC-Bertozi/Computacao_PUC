7698881496ea4591530ca203fc9817b3fa61d1a4
Maria Clara Bertozi <178790393+MC-Bertozi@users.noreply.github.com>
2024-10-04 12:03:32 -0300

    Add files via upload

---
+++ Segundo_Período(CC)/seminarios_react/04_10/MeusHobbies.jsx
@@ -0,0 +1,11 @@
+function MeusHobbies({hobbie1, hobbie2, hobbie3}){
+    return(
+        <div>
+            <h4>MEUS HOBBIES:</h4>
+            <p>-{hobbie1} <br></br>-{hobbie2} <br></br>-{hobbie3}</p>
+        </div>
+    )
+
+}
+
+export default MeusHobbies;
